f2b55427954ff4bae5fa8ee7a1d12092a162baae
Mikee Agpawan <magpawan418@west-mec.org>
2017-09-19 14:21:33 -0700

    Linked Results page with form
    Formaction

---
+++ form.js
@@ -0,0 +1,30 @@
+// 9-18-17
+
+
+"use strict";
+/*------------------------------------------Global Variables--------------------------------------------------------*/
+
+function createEventListeners() {
+  var form = document.getElementsByTagName("form")[0];
+  if (form.addEventListener) {
+    form.addEventListener('submit', validateForm, false);
+  } //End of if statement
+  //else if statement is for IE
+  else if (form.attachEvent) {
+    form.attachEvent('onsubmit', validateForm);
+  } //End of else if statement
+}
+
+
+
+/*-----------------------------------------------Load Event------------------------------------------------------------*/
+//Adding an event listener so it goes to the function
+//Will make sure the functions are working
+// if statement is for chrome
+if (window.addEventListener) {
+  window.addEventListener('load', createEventListeners, false);
+} //End of if statement
+//else if statement is for IE
+else if (window.attachEvent) {
+  window.attachEvent('onload', createEventListeners);
+} //End of else if statement
