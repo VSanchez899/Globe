194d7cec0c61b8f49c4e8767e7f5bdbc07818eb5
Vincent Nguyen <vincentnguyen201@yahoo.com>
2017-09-21 15:12:53 -0700

    Message

--- form.js
+++ form.js
@@ -16,7 +16,7 @@ var formValidity = true;
 function inputElements() {
   //Variables
   var inputElements = document.getElementsByTagName("input");
-  var errorDiv = document.getElementById("errorMessage");
+  var errorDiv = document.getElementById("errorText");
   var fieldsetValidity = true;
   var elementCount = inputElements.length; //Will count the value
   var currentElement; //Will count the value inputed by the user
@@ -24,20 +24,17 @@ function inputElements() {
 
   //Try Clause
   try {
-    console.log("inside try");
 
     for (var i = 0; i < inputElements.length; i++) {
         currentElement = inputElements[i];
         //Change the color of the box if the user has not inputed anything in - "" (Empty)
         if (currentElement.value === "") {
-          console.log("inside if");
           currentElement.style.background = "rgb(255, 100, 100)";
           fieldsetValidity = false;
         } //End of if statement
 
         else {
           //Color will not change and will stay white if nothing is wrong
-          console.log("inside else");
           currentElement.style.background = "white";
         } //End of else statement
       } //End of for statement
@@ -47,7 +44,6 @@ function inputElements() {
       //If the throw statement the for loop then it will break the code because it loops
       //through the error message as well error message as well which disrputs the code
       if (fieldsetValidity === false) {
-        console.log("inside the if message");
         throw "Please complete all the required fields."
       } //End of id statement
       else {
@@ -58,28 +54,22 @@ function inputElements() {
 
 
   catch(msg) {
-    console.log(msg);
+    // console.log(msg);
     errorDiv.innerHTML = msg;
     errorDiv.style.display = "block";
     formValidity = false;
   } //End of catch clause
-} //End of function inputElements()
-
+}
 /*----------------------------------------function selectGender--------------------------------------------------------*/
 function selectGender() {
+// alert("selectGender()");
 
-} //End of function selectGender()
-
-
-
+}
 /*------------------------------------------function formType--------------------------------------------------------*/
 function formType() {
+  // alert("formType()");
 
-} //End of function formType
-
-
-
-
+}
 /*------------------------------------------function messageBox--------------------------------------------------------*/
 function messageBox() {
   // alert("messageBox()");
@@ -111,10 +101,10 @@ function messageBox() {
 
 
 
-
 /*-----------------------------------------function validateForm--------------------------------------------------------*/
 function validateForm(evt) {
-  console.log("inside validateForm");
+
+  // console.log("inside validateForm");
 
   //Variables
   var form = document.getElementsByTagName("form")[0];
@@ -129,27 +119,32 @@ function validateForm(evt) {
 
   formValidity = true;
 
-  // VALIDITY TESTS HERE ------ MPK - 9/20/17
+  // VALIDITY TESTS HERE
   inputElements();
   formType();
+  selectGender();
   messageBox();
 
 
   //For a valid form
   if (formValidity === true) {
+
     document.getElementById("errorText").innerHTML = "";
     document.getElementById("errorText").style.display = "none";
     document.getElementsByTagName("form")[0].submit();
   } //End of if statement
 
-  //For an invalid form
+  //For a invalid form
   else { //Displays error messageBox
-    console.log(formValidity);
+
+    // console.log(formValidity);
+
+
     document.getElementById("errorText").innerHTML = "Please fix the indicated problems and then resubmit your order.";
     document.getElementById("errorText").style.display = "block";
     //scroll(0, 0);
   } //End of else statement
-} //End of function validateForm()
+}
 
 
 /*-------------------------------------function createEventListeners--------------------------------------------------------*/
@@ -167,7 +162,7 @@ function createEventListeners() {
 
 
 /*-----------------------------------------------Load Event------------------------------------------------------------*/
-//Adding an event listener so it goes through the functions
+//Adding an event listener so it goes to the function
 //Will make sure the functions are working
 // if statement is for chrome
 if (window.addEventListener) {
